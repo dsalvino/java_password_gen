0107b9e2be6478ff55d271567486a669d6077799
denis salvino <denis.salvino23@gmail.com>
2021-03-11 23:52:46 -0600

    Created the script to generate password filled with necessary code and stuff. but when pressing generate button the function is not called? debugging at the moment

---
+++ Develop/script.js
@@ -0,0 +1,59 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Write password to the #password input
+function writePassword() {
+  var userInputWant = [];
+  var collectedCharacters = [];
+
+  var uppercaseLetters = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
+  var lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm".split("");
+
+  var numbersCharacters = "1234567890".split();
+
+  var fancyCharacters = "\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~".split();
+
+  var lengthMessage = "Enter desired length of password (between 8 - 128 characters): ";
+  if (lengthMessage < 8) {
+    alert("Must be more than 8 characters!");
+    return;
+  }
+  if (lengthMessage > 128) {
+    alert("Must be less than 128 characters!");
+    return;
+  }
+
+  var confirmNumbers = confirm("Would you like to include Numbers in your password?");
+  var confirmLettersLower = confirm("Would you like to include lowercase letters in your password?");
+  var confirmLettersUpper = confirm("Would you like to include upperacse letters in your password?");
+  var confirmFancyCharacters = confirm("Would you like special characters in your password?");
+
+  if (confirmNumbers === true) {
+    collectedCharacters = collectedCharacters.concat(numbersCharacters);
+  }
+  if (confirmLettersLower === true) {
+    collectedCharacters = collectedCharacters.concat(lowercaseLetters);
+  }
+  if (confirmLettersUpper === true) {
+    collectedCharacters = collectedCharacters.concat(uppercaseLetters);
+  }
+  if (confirmFancyCharacters === true) {
+    collectedCharacters = collectedCharacters.concat(fancyCharacters);
+  }
+
+  for (var i = 0; i < lengthMessage; i++) {
+    userInputWant.push (combinedArray[Math.floor(Math.random() * collectedCharacters.length)]);
+  }
+  return userInputWant.join("");
+}
+
+function showPassword() {
+  var password = writePassword();
+  var endPassword = document.querySelector("#password");
+
+  endPassword.value = password;
+}
