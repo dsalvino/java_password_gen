30476cfea990e972b4c0d13be9753179371c726d
denis salvino <denis.salvino23@gmail.com>
2021-03-12 00:18:33 -0600

    I got it... gave the array that the stores what characters the user wants two different names on line 48. all good now

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 // Write password to the #password input
 function startPassword() {
   var userInputWant = [];
   var collectedCharacters = [];
-
+  
   var uppercaseLetters = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
   var lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm".split("");
-
+  
   var numbersCharacters = "1234567890".split();
-
+  
   var fancyCharacters = "\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~".split();
-
+  
   var lengthMessage = prompt("Enter desired length of password (between 8 - 128 characters): ");
   if (lengthMessage < 8) {
     alert("Must be more than 8 characters!");
@@ -26,12 +25,12 @@ function startPassword() {
     alert("Must be less than 128 characters!");
     return;
   }
-
+  
   var confirmNumbers = confirm("Would you like to include Numbers in your password?");
   var confirmLettersLower = confirm("Would you like to include lowercase letters in your password?");
   var confirmLettersUpper = confirm("Would you like to include upperacse letters in your password?");
   var confirmFancyCharacters = confirm("Would you like special characters in your password?");
-
+  
   if (confirmNumbers === true) {
     collectedCharacters = collectedCharacters.concat(numbersCharacters);
   }
@@ -44,16 +43,17 @@ function startPassword() {
   if (confirmFancyCharacters === true) {
     collectedCharacters = collectedCharacters.concat(fancyCharacters);
   }
-
+  
   for (var i = 0; i < lengthMessage; i++) {
-    userInputWant.push (combinedArray[Math.floor(Math.random() * collectedCharacters.length)]);
+    userInputWant.push (collectedCharacters[Math.floor(Math.random() * collectedCharacters.length)]);
   }
   return userInputWant.join("");
 }
 
 function writePassword() {
-  var endPassword = startPassword();
+  var password = startPassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = endPassword;
+  
+  passwordText.value = password;
 }
+generateBtn.addEventListener("click", writePassword);
